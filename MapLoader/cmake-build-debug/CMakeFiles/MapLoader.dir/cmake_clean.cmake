23a5895d70c5ca72de96df74223a49b04465828f
RuslanDallin <ruslan.dallin@outlook.com>
2020-09-27 02:36:22 -0400

    Creating the project

---
+++ MapLoader/cmake-build-debug/CMakeFiles/MapLoader.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MapLoader.dir/main.cpp.o"
+  "MapLoader"
+  "MapLoader.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MapLoader.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
