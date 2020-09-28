e7a7fba4d78a0b0c9183519cd2e2a7a5ae102d64
RuslanDallin <ruslan.dallin@outlook.com>
2020-09-27 21:35:18 -0400

    working contients

--- MapLoader/main.cpp
+++ MapLoader/main.cpp
@@ -9,30 +9,15 @@ using namespace std;
 
 class Continent{
 public:
-    Continent(int id, string name, int size, string color);
-    void print();
-
-    int getId() const;
-
-    void setId(int id);
-
-    const string &getName() const;
-
-    void setName(const string &name);
-
-    int getSize() const;
-
-    void setSize(int size);
-
-    const string &getColor() const;
-
-    void setColor(const string &color);
+    Continent(int idPar, string namePar, int sizePar, string colorPar);
+    static vector<Continent> extractContinents(const vector<string> &vParm);
 
 public:
     int id;
     std::string name;
     int size;
     std::string color;
+
 };
 
 class Territory {
@@ -54,62 +39,53 @@ Add all the continents and then add all the countries (do this by clicking the "
 Move all the countries to where they are located on the image (use the move tool and simply drag them)
 Use the join tool to join 2 countries together, do this for all neighbouring countries (to select none use right click).*/
 
-Continent::Continent(int id, string name, int size, string color){
-    id = id;
-    name = name;
-    size = size;
-    color = color;
-};
-
-int Continent::getId() const {
-    return id;
+Continent::Continent(int idPar, string namePar, int sizePar, string colorPar){
+    id = idPar;
+    name = namePar;
+    size = sizePar;
+    color = colorPar;
 }
 
-void Continent::setId(int id) {
-    Continent::id = id;
+vector<Continent> Continent::extractContinents(const vector<string> &v) {
+    cout << "extraction continents\n";
+    vector<Continent> continents;
+    bool flag;
+    string first;
+    string item;
+
+    int id = 1;
+    std::string name;
+    int size;
+    std::string color;
+    for (int i = 0; i < v.size(); i++){
+        std::stringstream line;
+        line << v[i];
+        line >> first;
+        //if (first.length() > 1 && first.at(0) == ';')
+        if (v[i].at(0) == ';' || v[i].at(0) == '\r' || v[i].at(0) == '\n')
+            continue;
+        if (flag && first == "[countries]")
+            break;
+        if (flag) {
+            std::stringstream cc;
+            cc << v[i];
+            cc >> name;
+            cc >> size;
+            cc >> color;
+            continents.push_back(Continent(id, name, size, color));
+            id++;
+        }
+        if (first == "[continents]")
+            flag = true;
+    }
+    cout << continents.size() << " extracted\n";
+    //for (auto i : continents) { cout << i.color << " , ";}
+    return continents;
 }
 
-const string &Continent::getName() const {
-    return name;
+std::ostream& operator<<(std::ostream &strm, const Continent &a) {
+    return strm << "[ " << a.id << " " << a.name << " " << a.size << " " << a.color << " ]";
 }
-
-void Continent::setName(const string &name) {
-    Continent::name = name;
-}
-
-int Continent::getSize() const {
-    return size;
-}
-
-void Continent::setSize(int size) {
-    Continent::size = size;
-}
-
-const string &Continent::getColor() const {
-    return color;
-}
-
-void Continent::setColor(const string &color) {
-    Continent::color = color;
-}
-
-void Continent::print(){
-    cout << id;
-    cout << name;
-    cout << size;
-    cout << color;
-};
-
-
-
-Territory::Territory(int id, std::string name, int contId, int x, int y){
-    id = id;
-    name = name;
-    contId = contId;
-    x = x;
-    y = y;
-};
-
 
 vector<string> split (const string &s, char delim) {
     vector<string> result;
@@ -122,52 +98,12 @@ vector<string> split (const string &s, char delim) {
 
     return result;
 }
-/*
-int main() {
-    string str = "adsf+qwer+poui+fdgh";
-    vector<string> v = split (str, '+');
 
-    for (auto i : v) cout << i << endl;
-
-    return 0;
-}*/
-
-
-/*
-int main( )
-{
-    std::ifstream input;
-    input.open("solar.map");
-    if (input.fail( ))
-    {
-        std::cout << "Input file opening failed.\n";
-        exit(1);
-    }
-
-    std::stringstream ss;
-    ss.str("");
-    ss.clear();
-
-    std::string s;
-    std::string next;
-    while (std::getline(input,s, '\n') ){
-        ss << s;
-    };
-
-    string str = ss.str();
-    vector<string> v = split (str, '\n');
-
-    for (auto i : v) cout << i << "\n";
-
-    return 0;
-
-}
-*/
 
 int main( )
 {
     std::ifstream input;
-    input.open("test.map");
+    input.open("europass.map");
     if (input.fail( ))
     {
         std::cout << "Input file opening failed.\n";
@@ -178,38 +114,13 @@ int main( )
     std::string s;
     while (std::getline(input,s) ){
         ss << s << "\n";
-    };
+    }
+
 
     vector<string> v = split (ss.str(), '\n');
-    vector<Continent> continents;
-    bool flag;
-    string item;
+    vector<Continent> vectorCont = Continent::extractContinents(v);
+    for (auto i : vectorCont) { cout << i << " \n";}
 
-    int id = 1;
-    std::string name;
-    int size;
-    std::string color;
-    for (int i = 0; i < v.size(); i++){
-        item = v[i];
-        if (item.length() > 1 && item.at(0) == ';')
-            continue;
-        if (flag) {
-            std::stringstream cc;
-            cc << item;
-            cc >> name;
-            cc >> size;
-            cc >> color;
-            continents.push_back(Continent(id, name, size, color));
-            id++;
-
-        }
-        if (item == "[continents]")
-            flag = true;
-    };
-
-    for (int i= 0; i < continents.size(); i++){
-        cout << continents[i].color;
-    };
 
 
     return 0;
